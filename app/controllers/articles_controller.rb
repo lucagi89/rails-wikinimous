cb0e715a05a2a5276d9a7929646c2de244763373
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-02-03 20:37:42 +0000

    start styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,25 +4,37 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
 
   def create
-
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 
   def edit
-
-  end
-
-  def show
     @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
 
+
   def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
