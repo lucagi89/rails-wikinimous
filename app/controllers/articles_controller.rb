e705829166edcf62864597242bdf1e36f996c9fc
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-02-03 16:29:31 +0000

    getting started

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,28 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
